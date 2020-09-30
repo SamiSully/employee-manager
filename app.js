0219af596b9e52c60411adb2271615a7a4bbf4ad
samis <samilsully@gmail.com>
2020-09-30 14:02:50 -0700

    All files are working and generated a test team

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+console.log(__dirname);
 const OUTPUT_DIR = path.resolve(__dirname, "output");
+console.log(OUTPUT_DIR);
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+console.log(outputPath);
 const render = require("./lib/htmlRenderer");
 const { inherits } = require("util");
 
@@ -98,7 +99,8 @@ const internQuestions = [
   },
 ];
 
-function startQuestions() {
+function init() {
+    console.log("Please generate your team below:");
   inquirer.prompt(startingQuestion).then(function (data) {
     console.log(data);
     if (data.addingTeamMember === true) {
@@ -130,7 +132,7 @@ function managerEntry() {
     );
     team.push(newManager);
     console.log(newManager);
-    startQuestions();
+    init();
   });
 }
 function internEntry() {
@@ -138,7 +140,7 @@ function internEntry() {
     var newIntern = new Intern(data.name, data.ID, data.email, data.school);
     team.push(newIntern);
     console.log(newIntern);
-    startQuestions();
+    init();
   });
 }
 function engineerEntry() {
@@ -146,40 +148,17 @@ function engineerEntry() {
     var newEngineer = new Engineer(data.name, data.ID, data.email, data.github);
     team.push(newEngineer);
     console.log(newEngineer);
-    startQuestions();
+    init();
   });
 }
 
 function endProgram() {
-  fs.writeFile("team.html", render(team), function (err) {
+  fs.writeFile(outputPath, render(team), function (err) {
     if (err) {
       console.log(err);
     }
     console.log("Your page has been printed!");
   });
-}
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-function init() {
-  console.log("Please generate your team below:");
-  startQuestions();
 }
 
 init();
